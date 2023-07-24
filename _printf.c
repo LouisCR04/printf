f11c0078734cfe123d724f3228f8be733a24e55a
LouisCR04 <louiskariuki04@gmail.com>
2023-07-24 12:34:05 +0300

    Initial

---
+++ _printf.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_printf - Prints
+ *@format: A pointer to a const char that specifies format
+ *
+ * Return: Number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+
+	va_start (args, n);
+
+	/* loop: va_arg(args, int)*/
+
+	va_end (args);
+	return (0);
+}
